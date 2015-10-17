3a9ca886ed9be3c27e6ac2e1553126c76978904c
Pedro Plowman <pedrofp@me.com>
2015-10-17 12:58:24 +1000

    Initial Commit

---
+++ P2EmailVerifier/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  P2EmailVerifier
+//
+//  Created by Pedro Plowman on 17/10/2015.
+//  Copyright © 2015 p2made. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
